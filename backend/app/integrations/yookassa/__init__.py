67b4c38a106c04e967d57fb927a06d1952be4b07
Арам <aramkog@gmail.com>
2025-12-11 02:21:45 +0300

    feat: add YooKassa Get Receipt integration

---
+++ backend/app/integrations/yookassa/__init__.py
@@ -0,0 +1,6 @@
+"""YooKassa интеграции."""
+from .get_receipt import YooKassaGetReceiptIntegration
+from app.integrations.registry import registry
+
+# Автоматическая регистрация интеграций
+registry.register(YooKassaGetReceiptIntegration())
