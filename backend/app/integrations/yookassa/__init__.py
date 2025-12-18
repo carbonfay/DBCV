29c589ae087a9e9b20f32cc200dcf6442502b88f
Арам <aramkog@gmail.com>
2025-12-18 14:57:16 +0300

    fix: remove duplicate registry call

--- backend/app/integrations/yookassa/__init__.py
+++ backend/app/integrations/yookassa/__init__.py
@@ -2,5 +2,4 @@
 from .get_receipt import YooKassaGetReceiptIntegration
 from app.integrations.registry import registry
 
-# Автоматическая регистрация интеграций
-registry.register(YooKassaGetReceiptIntegration())
+
