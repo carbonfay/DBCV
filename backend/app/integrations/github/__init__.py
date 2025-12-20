08f4135a9db63911d2f76dd128e742c2508eb70c
Кезин Денис <luxdenone@gmail.com>
2025-12-21 00:31:34 +0300

    сделал интеграцию create pull request

--- backend/app/integrations/github/__init__.py
+++ backend/app/integrations/github/__init__.py
@@ -2,11 +2,13 @@
 try:
     from .get_issue import GitHubGetIssueIntegration
     from .update_issue import GitHubUpdateIssueIntegration
+    from .create_pull_request import GitHubCreatePullRequestIntegration
     from app.integrations.registry import registry
 
     registry.register(GitHubGetIssueIntegration())
     registry.register(GitHubUpdateIssueIntegration())
-    all = ["GitHubGetIssueIntegration", "GitHubUpdateIssueIntegration"]
+    registry.register(GitHubCreatePullRequestIntegration())
+    all = ["GitHubGetIssueIntegration", "GitHubUpdateIssueIntegration", "GitHubCreatePullRequestIntegration"]
 except ImportError as e:
     # Если PyGithub не установлена, просто пропускаем
     import warnings
