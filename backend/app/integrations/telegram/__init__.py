552a1ac88e508dc87168ced2eca0e408b684c52e
Svetlana <svetlana@localhost.localdomain>
2025-12-20 12:43:00 +0300

    integration added telegram_send_document

--- backend/app/integrations/telegram/__init__.py
+++ backend/app/integrations/telegram/__init__.py
@@ -1,7 +1,6 @@
 """Telegram интеграции."""
 from .send_message import TelegramSendMessageIntegration
 from .send_document import TelegramSendDocumentIntegration
-from .get_updates import TelegramGetUpdatesIntegration
 from app.integrations.registry import registry
 
 # Автоматическая регистрация интеграций
