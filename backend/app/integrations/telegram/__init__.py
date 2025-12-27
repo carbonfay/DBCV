23b27ba1a2c34d6cc760ca7924b6653110159b0b
smackere42 <foxpain12@gmail.com>
2025-12-27 20:49:16 +0700

    Revert "feat(integration): add telegram forward_message integration"
    This reverts commit 1de74cba50511509e252cf44537f7c4321ac8733.

--- backend/app/integrations/telegram/__init__.py
+++ backend/app/integrations/telegram/__init__.py
@@ -1,9 +1,7 @@
 """Telegram интеграции."""
 from .send_message import TelegramSendMessageIntegration
-from .forward_message import TelegramForwardMessageIntegration
 from app.integrations.registry import registry
 
 # Автоматическая регистрация интеграций
 registry.register(TelegramSendMessageIntegration())
-registry.register(TelegramForwardMessageIntegration())
 
